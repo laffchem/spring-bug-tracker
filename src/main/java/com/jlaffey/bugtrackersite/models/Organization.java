26f3461c06b15dd25c27c7d78cd0759c1db84955
John Laffey <laffeychem@gmail.com>
2023-04-27 12:25:10 -0400

    add service mapper and repos

---
+++ src/main/java/com/jlaffey/bugtrackersite/models/Organization.java
@@ -0,0 +1,32 @@
+package com.jlaffey.bugtrackersite.models;
+import jakarta.persistence.*;
+
+
+import lombok.*;
+
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "organizations")
+public class Organization {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+
+    @OneToMany(mappedBy = "organization")
+    private List<Project> projects;
+
+}
