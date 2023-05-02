77527198e3025f0c98cce0a33f904904cff44723
John Laffey <laffeychem@gmail.com>
2023-05-02 11:05:51 -0400

    Fix login form

--- src/main/java/com/jlaffey/bugtrackersite/security/SecurityConfig.java
+++ src/main/java/com/jlaffey/bugtrackersite/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf().disable()
                 .authorizeRequests()
-                .requestMatchers("/login", "/register", "/projects", "/organizations", "/css", "/js")
+                .requestMatchers("/login", "/register", "/projects", "/organizations", "/css/**", "/js/**")
                 .permitAll()
                 .and()
                 .formLogin(form -> form
